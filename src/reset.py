c0d6e8050a016efe510775f1e27f43e618e8c020
gmgwgw <gorilla_a3@outlook.com>
2024-10-15 17:29:38 +0900

    wip

--- src/reset.py
+++ src/reset.py
@@ -2,21 +2,13 @@ import pyvisa
 
 rm = pyvisa.ResourceManager()
 visa_tuple = rm.list_resources()
-print(visa_tuple)
-usb_1 = visa_tuple[0]
-inst_1 = rm.open_resource(usb_1)
+inst = rm.open_resource(visa_tuple[0])
 
 # 機器の情報を取得
-inst_1.write("*IDN?")
-out_1 = inst_1.read()
+inst.write("*IDN?")
+out_1 = inst.read()
 print("IDN: ", out_1)
 
-# 機器の内部テストを実行
-inst_1.write("*TST?")
-out_2 = inst_1.read()
-print("TST: ", out_2)
-
-# TODO: 電圧かける
 # リセット
-inst_1.write("*RST")
+inst.write("*RST")
 print("RST")
