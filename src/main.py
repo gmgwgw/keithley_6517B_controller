37ad8d4cc30130bc8b2a83cb8e3df706c33df9d8
gmgwgw <gorilla_a3@outlook.com>
2024-10-15 17:30:10 +0900

    wip

--- src/main.py
+++ src/main.py
@@ -6,11 +6,11 @@ if __name__ == "__main__":
     inst = rm.open_resource(visa_tuple[0])
 
     # 機器の情報を取得
-    inst_1.write("*IDN?")
-    out_1 = inst_1.read()
+    inst.write("*IDN?")
+    out_1 = inst.read()
     print("IDN: ", out_1)
 
     # 機器の内部テストを実行
-    inst_1.write("*TST?")
-    out_2 = inst_1.read()
+    inst.write("*TST?")
+    out_2 = inst.read()
     print("TST: ", out_2)
